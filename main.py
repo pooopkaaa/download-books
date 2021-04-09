8ec29018a9b319be7d2a1fe0275ff8ee81e03efe
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-09 18:04:13 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import requests
 import urllib3
+from pathlib import Path
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def main():
-    url = 'https://tululu.org/txt.php?id=3216834234234'
-    response = requests.get(url, verify=False)
-    with open('book.txt', 'wb') as file:
-        file.write(response.content)
+    directory_books_name = "books"
+    books_amount = 10
+    Path(directory_books_name).mkdir(exist_ok=True)
+
+    for book_id in range(1, books_amount + 1):
+        url = f'https://tululu.org/txt.php?id={book_id}'
+        response = requests.get(url, verify=False)
+        with open(f'{directory_books_name}/id{book_id}.txt', 'wb') as file:
+            file.write(response.content)
 
 
 if __name__ == '__main__':
