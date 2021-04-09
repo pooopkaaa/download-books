1762b2d2c4364e2228d2592fed6e363a5411a89d
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-09 21:20:38 +0300

    Check redirect

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from pathlib import Path
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/':
+        raise requests.exceptions.HTTPError(response.url)
+
+
 def main():
     directory_books_name = "books"
     books_amount = 10
@@ -13,8 +18,14 @@ def main():
     for book_id in range(1, books_amount + 1):
         url = f'https://tululu.org/txt.php?id={book_id}'
         response = requests.get(url, verify=False)
-        with open(f'{directory_books_name}/id{book_id}.txt', 'wb') as file:
-            file.write(response.content)
+
+        try:
+            check_for_redirect(response)
+            with open(f'{directory_books_name}/id{book_id}.txt', 'wb') as file:
+                file.write(response.content)
+        except requests.exceptions.HTTPError as redirect_error:
+            print(f'Url: {url} -> не содержит файла.
+                  Происходит переадресация на {redirect_error}')
 
 
 if __name__ == '__main__':
