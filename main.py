c52c9fb6074b3618d31e49f46aba1d96a18fc606
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-09 22:27:55 +0300

    Get book title and author

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import requests
 import urllib3
 from pathlib import Path
+import requests
+from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def parse_book_page(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_title, book_author = [book_characteristic.strip() for book_characteristic in soup.find('h1').text.split('::')]
+    return book_title, book_author
 
 
 def check_for_redirect(response):
@@ -12,20 +19,22 @@ def check_for_redirect(response):
 
 def main():
     directory_books_name = "books"
-    books_amount = 10
+    books_amount = 1
     Path(directory_books_name).mkdir(exist_ok=True)
 
     for book_id in range(1, books_amount + 1):
-        url = f'https://tululu.org/txt.php?id={book_id}'
+        url = f'https://tululu.org/b{book_id}/'
         response = requests.get(url, verify=False)
 
         try:
             check_for_redirect(response)
+            book_title, book_author = parse_book_page(response)
+            print(f'Заголовок: {book_title}\nАвтор: {book_author}')
             with open(f'{directory_books_name}/id{book_id}.txt', 'wb') as file:
                 file.write(response.content)
         except requests.exceptions.HTTPError as redirect_error:
-            print(f'Url: {url} -> не содержит файла.
-                  Происходит переадресация на {redirect_error}')
+            print(f'Url: {url} -> не содержит файла.\
+            Происходит переадресация на {redirect_error}')
 
 
 if __name__ == '__main__':
