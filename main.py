af9c5ec4e9262c7431ee161c663c6888d2654653
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-04-10 11:44:04 +0300

    Update main.py using PEP8

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import urllib3
 import os
+import urllib3
+from urllib.parse import urljoin
+from urllib.parse import urlsplit
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-from urllib.parse import urljoin
-from urllib.parse import urlsplit
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -39,10 +40,17 @@ def download_txt(url, filename, folder='books/'):
 
 def parse_book_page(response):
     soup = BeautifulSoup(response.text, 'lxml')
-    book_title, book_author = [book_characteristic.strip() for book_characteristic in soup.find('h1').text.split('::')]
+    book_title, book_author = [book_property.strip() for book_property in soup
+                               .find('h1')
+                               .text
+                               .split('::')]
     book_img_src = soup.find('div', class_='bookimage').img['src']
-    book_comments = [book_comment.span.text for book_comment in soup.find_all('div', class_='texts')]
-    book_genres = [book_genre.text for book_genre in soup.find('span', class_='d_book').find_all('a')]
+    book_comments = [book_comment.span.text for book_comment in soup
+                     .find_all('div', class_='texts')]
+    book_genres = [book_genre.text for book_genre in soup
+                   .find('span', class_='d_book')
+                   .find_all('a')]
+
     return {'book_title': book_title,
             'book_author': book_author,
             'book_img_src': book_img_src,
@@ -58,29 +66,31 @@ def check_for_redirect(response):
 def main():
     folder_book_name = "books/"
     folder_img_name = "images/"
-    books_amount = 10
+    page_amount = 10
 
-    for book_id in range(1, books_amount + 1):
-        url = f'https://tululu.org/b{book_id}/'
+    for page_id in range(1, page_amount + 1):
+        page_url = f'https://tululu.org/b{page_id}/'
         try:
-            response = get_resonse(url)
-
-            book_url = f'https://tululu.org/txt.php?id={book_id}'
+            response = get_resonse(page_url)
             book_description = parse_book_page(response)
+
+            book_url = f'https://tululu.org/txt.php?id={page_id}'
             book_title = book_description['book_title']
             book_img_src = book_description['book_img_src']
             book_genres = book_description['book_genres']
-            book_filename = f'{book_id}.{book_title}.txt'
-            # book_filepath = download_txt(book_url, book_filename, folder_book_name)
-
+            book_filename = f'{page_id}.{book_title}.txt'
             book_img_url = urljoin(book_url, book_img_src)
             img_filename = f"{urlsplit(book_img_url).path.split('/')[-1]}"
-            # img_filepath = download_image(book_img_url, img_filename, folder_img_name)
 
-            print(book_genres)
+            book_filepath = download_txt(book_url,
+                                         book_filename,
+                                         folder_book_name)
+            img_filepath = download_image(book_img_url,
+                                          img_filename,
+                                          folder_img_name)
+
         except requests.exceptions.HTTPError as redirect_error:
-            print(f'Url: {url} -> не содержит файла.\
-            Происходит переадресация на {redirect_error}')
+            print(f'{page_url} -> переадресация на {redirect_error}')
 
 
 if __name__ == '__main__':
