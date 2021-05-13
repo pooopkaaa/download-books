a1f9c0dd1467397ed579491101ad1f3c2f89e8d4
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-13 21:43:44 +0300

    Update function to get book urls from pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -18,19 +18,23 @@ def get_response(url, payload=[]):
     return response
 
 
-def get_book_url(response):
+def get_book_urls(response):
     site_url = 'https://tululu.org/'
     soup = BeautifulSoup(response.text, 'lxml')
-    book_card = soup.find('table', class_='d_book')
-    book_url = urljoin(site_url, book_card.find('a')['href'])
-    return book_url
+    book_cards = soup.find_all('table', class_='d_book')
+    book_urls = [urljoin(site_url, book_card.find('a')['href']) for book_card in book_cards]
+    return book_urls
 
 
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    response = get_response('https://tululu.org/l55/')
-    book_url = get_book_url(response)
-    print(book_url)
+    books_page_amount = 10
+    for books_page_number in range(1, books_page_amount + 1):
+        books_page_url = f'https://tululu.org/l55/{books_page_number}'
+        books_page = get_response(books_page_url)
+        book_urls = get_book_urls(books_page)
+        for book_url in book_urls:
+            print(book_url)
 
 
 if __name__ == '__main__':
