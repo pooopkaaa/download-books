40edd60ef54b70380f73c9f61cec6b89ad08b524
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-19 17:00:35 +0300

    Save book description to file json

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import urllib3
+import json
 from urllib.parse import urljoin, urlsplit, unquote
 from pathlib import Path
 
@@ -11,16 +12,6 @@ from pathvalidate import sanitize_filename
 
 def get_command_line_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s',
-                        '--start_id',
-                        default='1',
-                        type=int,
-                        help='С какой страницы скачивать')
-    parser.add_argument('-e',
-                        '--end_id',
-                        default='10',
-                        type=int,
-                        help='По какую страницу скачивать')
     parser.add_argument('-b',
                         '--book',
                         default='books/',
@@ -31,12 +22,30 @@ def get_command_line_args():
                         default='images/',
                         type=str,
                         help='Куда сохранять обложки')
+    parser.add_argument('-d',
+                        '--description',
+                        default='books',
+                        type=str,
+                        help='Название файла с описанием книг')
     return parser.parse_args()
 
 
 def check_for_redirect(response):
     if response.url == 'https://tululu.org/':
         raise requests.exceptions.URLRequired(response.url)
+
+
+def create_json_file(filename):
+    with open(filename, 'w') as file:
+        file.write('[]')
+
+
+def save_book_description(book_description, filename):
+    with open(filename, 'r+', encoding='utf-8') as file:
+        books_description = json.load(file)
+        books_description.append(book_description)
+        file.seek(0)
+        json.dump(books_description, file, ensure_ascii=False, indent=4)
 
 
 def download_txt(url, payload, filename, folder):
@@ -67,11 +76,11 @@ def parse_book_page(response):
                    .find('span', class_='d_book')
                    .find_all('a')]
 
-    return {'book_title': book_title.strip(),
-            'book_author': book_author.strip(),
+    return {'title': book_title.strip(),
+            'author': book_author.strip(),
             'book_img_src': book_img_src,
-            'book_comments': book_comments,
-            'book_genres': book_genres}
+            'comments': book_comments,
+            'genres': book_genres}
 
 
 def get_response(url, payload=[]):
@@ -91,8 +100,13 @@ def get_book_hrefs(response):
 def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     command_line_args = get_command_line_args()
+
     Path(command_line_args.book).mkdir(exist_ok=True)
     Path(command_line_args.image).mkdir(exist_ok=True)
+
+    books_description_filename = command_line_args.description + '.json'
+    if not os.path.exists(books_description_filename):
+        create_json_file(books_description_filename)
 
     books_page_amount = 10
     site_url = 'https://tululu.org/'
@@ -100,7 +114,7 @@ def main():
 
     for books_page_number in range(1, books_page_amount + 1):
         books_page_url = f'https://tululu.org/l55/{books_page_number}'
-        
+
         print(f'Номер страницы {books_page_number}')
 
         books_page = get_response(books_page_url)
@@ -108,35 +122,42 @@ def main():
 
         for book_href in book_hrefs:
             try:
-                book_id = book_href.replace('b', '').replace('/','')
+                book_id = book_href.replace('b', '').replace('/', '')
                 book_url = urljoin(site_url, book_href)
                 book_page = get_response(book_url)
                 book_description = parse_book_page(book_page)
 
-                book_title = book_description['book_title']
-                book_author = book_description['book_author']
-                book_img_src = book_description['book_img_src']
-                book_genres = book_description['book_genres']
-                book_comments = book_description['book_comments']
+                book_title = book_description['title']
+                book_img_src = book_description.pop('book_img_src')
 
                 book_txt_payload = {'id': book_id}
                 book_filename = f'{book_id}.{book_title}.txt'
-                book_filepath = download_txt(book_txt_url,
-                                            book_txt_payload,
-                                            book_filename,
-                                            command_line_args.book)
+                book_filepath = download_txt(
+                    book_txt_url,
+                    book_txt_payload,
+                    book_filename,
+                    command_line_args.book
+                )
 
                 book_img_url = urljoin(book_url, book_img_src)
                 img_filename = f"{unquote(urlsplit(book_img_url).path.split('/')[-1])}"
-                img_filepath = download_image(book_img_url,
-                                            img_filename,
-                                            command_line_args.image)
-                
+                img_filepath = download_image(
+                    book_img_url,
+                    img_filename,
+                    command_line_args.image
+                )
+
+                book_description['img_src'] = img_filepath
+                book_description['book_path'] = book_filepath
+
+                save_book_description(book_description, books_description_filename)
+
                 print(f'Ссылка на книгу {book_url}')
             except requests.exceptions.URLRequired as redirect_error:
                 print(f'{book_url} -> переадресация на {redirect_error}')
             except requests.exceptions.HTTPError as http_error:
                 print(f'Ответ пришел с ошибкой -> {http_error}')
+
 
 if __name__ == '__main__':
     main()
