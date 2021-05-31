5cb090fb988c62c61a383ce29cb06da0be46a555
pooopkaaa <60960322+pooopkaaa@users.noreply.github.com>
2021-05-31 18:18:36 +0300

    Fix code using PEP8

--- render_website.py
+++ render_website.py
@@ -3,42 +3,59 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 
 import json
+from pathlib import Path
 import os
 
 
-def on_reload():
+def read_book_descriptions_file(book_descriptions_filename):
     book_descriptions_filename = 'descriptions.json'
+    with open(book_descriptions_filename, 'r', encoding='utf-8') as file:
+        json_book_descriptions = file.read()
+    book_descriptions = json.loads(json_book_descriptions)
+    return book_descriptions
+
+
+def group_book_descriptions(book_descriptions):
+    max_columns_count = 2
+    max_rows_count = 10
+    chunked_row_book_descriptions = list(chunked(book_descriptions, max_columns_count))
+    grouped_book_descriptions = list(chunked(chunked_row_book_descriptions, max_rows_count))
+    return grouped_book_descriptions
+
+
+def render_page(grouped_book_descriptions, template):
+    pages_amount = len(grouped_book_descriptions)
+    for page, grouped_book_description in enumerate(grouped_book_descriptions, start=1):
+        rendered_page = template.render(
+            grouped_book_description=grouped_book_description,
+            pages_amount=pages_amount,
+            current_page=page)
+        filename = os.path.join('pages', f'index{page}.html')
+        with open(filename, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
+
+def on_reload():
+    filepath_pages = Path('pages')
+    filepath_pages.mkdir(exist_ok=True)
     template_filename = 'template.html'
+    book_descriptions_filename = 'descriptions.json'
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
     template = env.get_template(template_filename)
 
-    with open('descriptions.json', 'r', encoding='utf-8') as file:
-        json_book_descriptions = file.read()
-    book_descriptions = json.loads(json_book_descriptions)
-
-    chunked_for_row_book_descriptions = list(chunked(book_descriptions, 2))
-    chunked_for_page_book_descriptions = list(chunked(chunked_for_row_book_descriptions, 10))
-    pages_amount = len(chunked_for_page_book_descriptions)
-    print(pages_amount)
-    for page, chunked_for_page_book_description in enumerate(chunked_for_page_book_descriptions, start=1):
-        rendered_page = template.render(
-            chunked_for_page_book_description=chunked_for_page_book_description,
-            pages_amount=pages_amount, 
-            current_page=page)
-        filename = os.path.join('pages', f'index{page}.html')
-        with open(filename, 'w', encoding="utf8") as file:
-            file.write(rendered_page)
-
-    server = Server()
-    server.watch('*.rst', shell('make html', cwd='docs'))
-    server.serve()
+    book_descriptions = read_book_descriptions_file(book_descriptions_filename)
+    grouped_book_descriptions = group_book_descriptions(book_descriptions)
+    render_page(grouped_book_descriptions, template)
 
 
 def main():
-    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve()
 
 
 if __name__ == '__main__':
